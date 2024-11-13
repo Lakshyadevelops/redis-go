a0f6864a3cb32cd527f7956ff2829518cf0ff5ff
Lakshya Garg <lakshyarajgarg@gmail.com>
2024-11-13 18:04:18 +0530

    implement ECHO and RESP parser

--- app/server.go
+++ app/server.go
@@ -6,7 +6,32 @@ import (
 	"io"
 	"net"
 	"os"
+	"strings"
 )
+
+func ping() ([]byte, error) {
+	resp := "+PONG\r\n"
+	return []byte(resp), nil
+}
+
+func echo(input []string) ([]byte, error) {
+	text := "+" + input[1][:] + "\r\n"
+	return []byte(text), nil
+}
+
+func respParser(data []byte) ([]byte, error) {
+	input_array := strings.Split(string(data), "\r\n")
+
+	cmd := strings.ToUpper(input_array[2*1][:])
+	switch cmd {
+	case "ECHO":
+		return echo(input_array[3:])
+	case "PING":
+		return ping()
+	default:
+		return nil, errors.ErrUnsupported
+	}
+}
 
 func main() {
 	fmt.Println("Logs from your program will appear here!")
@@ -29,11 +54,15 @@ func main() {
 			defer con.Close()
 
 			data := make([]byte, 256)
-			resp := "+PONG\r\n"
 			for {
-				_, err = con.Read(data)
+				_, err := con.Read(data)
 				if errors.Is(err, io.EOF) {
 					return
+				}
+				resp, err := respParser(data)
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(1)
 				}
 				con.Write([]byte(resp))
 			}
