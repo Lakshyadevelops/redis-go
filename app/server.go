920d4e4ba3022265d405ea612e5fcd353f1d49ae
Lakshya Garg <lakshyarajgarg@gmail.com>
2024-11-28 17:42:34 +0530

    replication support sending RDB

--- app/server.go
+++ app/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/binary"
+	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"flag"
@@ -343,7 +344,16 @@ func replconf_cmd(input []string) ([]byte, error) {
 }
 
 func psync_cmd(input []string) ([]byte, error) {
-	return []byte(fmt.Sprintf("+FULLRESYNC %v %v\r\n", _info.Master_replid, _info.Master_repl_offset)), nil
+	// TO DO : Allow Sending Any RDB
+	
+	// SAMPLE RDB
+	rdb := "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
+	decodedBytes, _ := hex.DecodeString(rdb)
+	resp := make([]byte, 0, 512)
+	resp = append(resp, []byte(fmt.Sprintf("+FULLRESYNC %v %v\r\n$%v\r\n", _info.Master_replid, _info.Master_repl_offset, len(decodedBytes)))...)
+	resp = append(resp, decodedBytes...)
+	fmt.Println(resp)
+	return resp, nil
 }
 
 func respParser(data []byte) ([]byte, error) {
